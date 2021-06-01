63770714cee8bb4ddb4bba15f49a3522c3f911a6
CeciliaSilvaSandoval <81256949+CeciliaSilvaSandoval@users.noreply.github.com>
2021-06-01 03:38:56 -0500

    Asignacion 20-05-2021 autentificacion con token y njwt, inicios de creacion de formulario

---
+++ E2021-05-20/express-app1/src/routes/auth.route.ts
@@ -0,0 +1,48 @@
+import { Request, Response, NextFunction } from 'express';
+var nJwt = require('njwt');
+
+import { handleAsync } from '../shared/utilities';
+import { IService } from '../services/index.service';
+import { IRoute, Route } from './index.route';
+import { validationMiddleware } from '../middleware/validation.middleware';
+import { AuthUserValidator } from '../models/users.validator';
+
+class AuthRoute extends Route {
+
+    register = (api: string, service: IService): IRoute => {
+
+        this.api = api;
+        this.service = service;
+
+        this.router.post(`/`, validationMiddleware(AuthUserValidator), this.post);
+        return this;
+    }
+    protected post = async (request: Request, response: Response, next: NextFunction) => {
+
+        const data = request.body;
+
+        let [items, error] = await handleAsync(this.service.find(data));
+
+        if (error) return next(error);
+
+        if (items.length) {
+            //User is authenticated
+            //Create a jwt token & SEND IT ALOING WITH THE RESPONESE 
+            const payload = items[0].id;
+            const scope = `admin`;
+            const claims = {
+                iss: 'ejAmerica.com',  // The URL of your service
+                sub: payload,    // The UID of the user in your system
+                scope: scope
+            }
+            const token= nJwt.create(claims,`xxx`);
+            token.setExpiration(new Date().getTime()+60*2000);//2 min 
+
+            //append token to items 
+            items.push({token: token.compact()});
+        }
+        response.json(items);
+    }
+}
+
+export default AuthRoute;
---
+++ E2021-05-20/readme.md
@@ -0,0 +1,8 @@
+# Asignacion 20-05-2021 autentificacion con token y njwt, inicios de creacion de formulario
+https://www.npmjs.com/package/njwt
+https://jwt.io/ 
+
+![Alt text](img1.png)
+![Alt text](img2.png)
+![Alt text](img3.png)
+![Alt text](img4.png)
